455de9031835c5a73469c72e1ad25e39030c01ea
MauroFar <mauro_far@outlook.com>
2025-06-30 09:29:34 -0500

    FUNCIONAL modificado serverjs para probar en prodcucion

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,11 @@ require('dotenv').config();
 
 // Configuración de CORS
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: [
+    'http://localhost:5173',
+    'http://192.168.130.192'
+    
+  ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
