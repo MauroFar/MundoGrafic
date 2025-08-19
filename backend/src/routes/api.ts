7263ed877c642c72229c42469e5d6fc6b580afe4
MauroFar <mauro_far@outlook.com>
2025-08-19 13:10:36 -0500

    cotizacion routes ver si es problema12

--- backend/src/routes/api.ts
+++ backend/src/routes/api.ts
@@ -4,7 +4,7 @@ import rucRoutes from "./rucs";
 import clientesRoutes from "./clientes";
 import cotizacionRoutes from "./cotizaciones";
 import cotizacionDetRoutes from "./cotizacionesDetalles";
-// import cotizacionesEditar from "./cotizacionesEditar";    // TEMPORALMENTE COMENTADO
+import cotizacionesEditar from "./cotizacionesEditar";
 // import ordenTrabajoRoutes from "./ordenTrabajo";    // TEMPORALMENTE COMENTADO
 import puppeteer from "puppeteer";
 // import uploadRoutes from "./upload";                // TEMPORALMENTE COMENTADO
@@ -18,7 +18,7 @@ export default (client: any) => {
   router.use("/clientes", clientesRoutes(client)); // /api/clientes
   router.use("/cotizaciones", cotizacionRoutes(client)); // /api/cotizaciones
   router.use("/cotizacionesDetalles", cotizacionDetRoutes(client)); // /api/cotizacionesDetalles
-  // router.use("/cotizacionesEditar", cotizacionesEditar(client));
+  router.use("/cotizacionesEditar", cotizacionesEditar(client));
   // router.use("/ordenTrabajo", ordenTrabajoRoutes(client)); // /api/ordenTrabajo
   // router.use("/upload", uploadRoutes); // /api/upload
   // router.use("/chat", chatRoutes(client)); // /api/chat
